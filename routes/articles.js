061f34dfdd0a2cf09afa170d5702baf8baca8d02
Carolina Toren <54749413+Carolina-Toren@users.noreply.github.com>
2022-08-05 23:59:56 +0300

    changed articles text length

--- routes/articles.js
+++ routes/articles.js
@@ -1,40 +1,43 @@
 const express = require('express');
 
 const router = express.Router();
-const { celebrate, Joi } = require('celebrate');
-const { validateURL } = require('../helpers/validator');
+const {celebrate, Joi} = require('celebrate');
+const {validateURL} = require('../helpers/validator');
 
-const { getAllArticles, createArticle, deleteArticle } = require('../controllers/articles');
+const {
+	getAllArticles,
+	createArticle,
+	deleteArticle,
+} = require('../controllers/articles');
 
 router.get('/', getAllArticles);
 
 router.post(
-  '/',
-  celebrate({
-    body: Joi.object().keys({
-      keyword: Joi.string().required().min(2).max(30),
-      text: Joi.string().required().min(2).max(30),
-      title: Joi.string().required().min(2).max(30),
-      date: Joi.string().required().min(6).max(10),
-      source: Joi.string().required().min(2).max(30),
-      link: Joi.string().required().custom(validateURL),
-      image: Joi.string().required().custom(validateURL),
-
-    }),
-  }),
-  createArticle,
+	'/',
+	celebrate({
+		body: Joi.object().keys({
+			keyword: Joi.string().required().min(2).max(30),
+			text: Joi.string().required().min(2),
+			title: Joi.string().required().min(2),
+			date: Joi.string().required().min(6).max(20),
+			source: Joi.string().required().min(2).max(30),
+			link: Joi.string().required().custom(validateURL),
+			image: Joi.string().required().custom(validateURL),
+		}),
+	}),
+	createArticle
 );
 
 router.delete(
-  '/:articlesId',
-  celebrate({
-    params: Joi.object()
-      .keys({
-        articlesId: Joi.string().hex().length(24),
-      })
-      .unknown(true),
-  }),
-  deleteArticle,
+	'/:articlesId',
+	celebrate({
+		params: Joi.object()
+			.keys({
+				articlesId: Joi.string().hex().length(24),
+			})
+			.unknown(true),
+	}),
+	deleteArticle
 );
 
 module.exports = router;
